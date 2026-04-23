87d3b3a919d46125e298911237b77938de494df4
liner.exe <90984207+liner-exe@users.noreply.github.com>
2026-04-23 19:16:12 +0300

    Added custom element

--- app/src/main/java/com/liner_exe/smartcart/fragments/FragmentSettings.java
+++ app/src/main/java/com/liner_exe/smartcart/fragments/FragmentSettings.java
@@ -4,11 +4,15 @@ import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.liner_exe.smartcart.R;
 import com.liner_exe.smartcart.databinding.FragmentSettingsBinding;
 
 public class FragmentSettings extends Fragment {
@@ -18,6 +22,12 @@ public class FragmentSettings extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentSettingsBinding.inflate(inflater, container, false);
+
+        binding.productsManagementButton.setOnClickListener(view -> {
+            NavController navController = Navigation
+                    .findNavController(requireActivity(), R.id.main_nav_host);
+            navController.navigate(R.id.action_mainFragment_to_productsManagementFragment);
+        });
 
         binding.aboutButton.setOnClickListener(view -> {
             showAboutDialog();
