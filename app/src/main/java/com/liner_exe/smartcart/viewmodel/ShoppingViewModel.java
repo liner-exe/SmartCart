db7467ac90ff840f0216b2ec0ccf8d73f1cb8ba8
liner.exe <90984207+liner-exe@users.noreply.github.com>
2026-04-24 12:07:18 +0300

    ViewModel: added new fields and methods

--- app/src/main/java/com/liner_exe/smartcart/viewmodel/ShoppingViewModel.java
+++ app/src/main/java/com/liner_exe/smartcart/viewmodel/ShoppingViewModel.java
@@ -1,7 +1,29 @@
 package com.liner_exe.smartcart.viewmodel;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class ShoppingViewModel extends ViewModel {
+import com.liner_exe.domain.models.Product;
+import com.liner_exe.domain.repository.IShoppingRepository;
 
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ShoppingViewModel extends ViewModel {
+    private final IShoppingRepository repository;
+
+    private final ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+    private final MutableLiveData<List<Product>> _products = new MutableLiveData<>();
+    public LiveData<List<Product>> products = _products;
+
+    public ShoppingViewModel(IShoppingRepository repository) {
+        this.repository = repository;
+    }
+
+    public void loadProducts() {
+
+    }
 }
