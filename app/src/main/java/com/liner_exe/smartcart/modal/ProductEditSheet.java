14c3c0badc924f42bb83b140155dd197135f644c
liner.exe <90984207+liner-exe@users.noreply.github.com>
2026-05-04 15:45:53 +0300

    Added BottomSheetDialog

---
+++ app/src/main/java/com/liner_exe/smartcart/modal/ProductEditSheet.java
@@ -0,0 +1,31 @@
+package com.liner_exe.smartcart.modal;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.liner_exe.domain.models.Product;
+
+public class ProductEditSheet extends BottomSheetDialogFragment {
+    public static ProductEditSheet newInstance(Product product) {
+        ProductEditSheet sheet = new ProductEditSheet();
+
+        return sheet;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
